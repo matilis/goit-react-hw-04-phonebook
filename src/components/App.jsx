0f2f25d49ca086e8be1c266b3ef9d5f618844090
Mateusz Lis <mateusz.a.lis@gmail.com>
2023-07-03 15:13:47 +0200

    HW. 4.2 Finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
 export const App = () => {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('Contacts_Local_Storage')) || []
+  );
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('Contacts_Local_Storage', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addNewName = (name, number) => {
+    const contactNames = contacts.map(contact => contact.name);
+
+    if (contactNames.includes(name))
+      return alert(`${name} is alredy in contacts`);
+
+    setContacts([...contacts, { id: nanoid(), name, number }]);
+  };
+
+  const showFilteredContacts = () =>
+    contacts.filter(showContact =>
+      showContact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+  const deleteContact = id =>
+    setContacts(contacts.filter(contact => contact.id !== id));
+
+  const handleFilterChange = newValue => setFilter(newValue);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={addNewName} />
+      <h2>Contacts</h2>
+      <Filter onChange={handleFilterChange} />
+      <ContactList contacts={showFilteredContacts()} onClick={deleteContact} />
     </div>
   );
 };
