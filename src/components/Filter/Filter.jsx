32745a46918bfcc343550e97eafe14151f14dea5
Mateusz Lis <mateusz.a.lis@gmail.com>
2023-07-03 16:39:13 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,8 +1,10 @@
 import css from './Filter.module.css';
 import PropTypes from 'prop-types';
 
-export const Filter = ({ onChange }) => {
-  const filterContacts = event => onChange(event.target.value);
+export const Filter = filter => {
+  const { onChange } = filter;
+
+  const filterContacts = e => onChange(e.target.value);
   return (
     <div className={css.filter}>
       <h3>Find contact by name</h3>
